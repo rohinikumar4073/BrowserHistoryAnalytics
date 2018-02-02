d8a5ea11275f6a98fcfb06c697724b85705a7cb5
vishnu sandhireddy <vissu_nbkr@yahoo.com>
2018-02-02 08:25:07 +0530

    chnaged to google charts

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -3,7 +3,26 @@ let w = 300,                        //width
   r = 150,                            //radius
   color = d3.scale.category20c();
 const domSelector = "#browserhistory__piechart";
-class Charts {
+
+class D3Charts {
+  static colorPaltter(n) {
+    const colores_g = ["#FCE4EC", "#F8BBD0", "#F48FB1", "#F06292",
+      "#EC407A", "#E91E63", "#D81B60", "#D81B60",
+      "#D81B60", "#D81B60", "#D81B60", "#D81B60",
+      "#D81B60", "#D81B60"
+    ];
+    return colores_g[n % colores_g.length];
+  }
+  static purpleColors(n) {
+    const colores_g = ["#EDE7F6", "#D1C4E9", "#B39DDB",
+      "#9575CD", "#7E57C2", "#673AB7", "#5E35B1",
+      "#512DA8", "#4527A0", "#311B92", "#B388FF",
+      "#7C4DFF", "#651FFF", "#6200EA"
+    ];
+    return colores_g[(colores_g.length - n - 1) % colores_g.length];
+  }
+
+
   static loadCharts(data, selector) {
     let vis = d3.select(selector).html("")
       .append("svg:svg")
@@ -21,17 +40,32 @@ class Charts {
       .append("svg:g")
       .attr("class", "slice");
     arcs.append("svg:path")
-      .attr("fill", function (d, i) { return color(i); })
+      .attr("fill", function (d, i) { return Charts.purpleColors(i); })
       .attr("d", arc);
-    arcs.append("svg:text")                                     //add a label to each slice
-      .attr("transform", function (d) {                    //set the label's origin to the center of the arc
-        //we have to make sure to set these before calling arc.centroid
+    arcs.append("svg:text")
+      .attr("transform", function (d) {
         d.innerRadius = 0;
         d.outerRadius = r;
-        return "translate(" + arc.centroid(d) + ")";        //this gives us a pair of coordinates like [50, 50]
+        return "translate(" + arc.centroid(d) + ")";
       })
-      .attr("text-anchor", "middle")                          //center the text on it's origin
+      .attr("text-anchor", "middle")
       .text(function (d, i) { return data[i].label; });
+  }
+}
+google.charts.load('current', {'packages':['corechart']});
+
+class GoogleCharts{
+
+  
+  static loadCharts(data, selector) {
+
+    var data = google.visualization.arrayToDataTable(data);
+    var options = {
+      title: 'Browsing History'
+    };
+    var chart = new google.visualization.PieChart(document.getElementById('browserhistory__piechart'));
+    chart.draw(data, options);
+
   }
 }
 class BrowserHistory {
@@ -40,7 +74,7 @@ class BrowserHistory {
     chrome.history.search({
       text: '', startTime: startTime, endTime: endTime, maxResults: 150000
     }, function (data) {
-      BrowserHistory.processHistory(data)
+      BrowserHistory.processGoogleChartsHistory(data)
     });
   }
   static getStartEndDate(noOfDays) {
@@ -49,7 +83,8 @@ class BrowserHistory {
     let startTime = today.getTime() - 24 * 60 * 60 * 1000 * noOfDays;
     return [startTime, endTime];
   }
-  static processHistory(data) {
+
+  static getMaps(data){
     let maps = {};
     data.forEach(element => {
       const hostname = new URL(element.url).hostname;
@@ -59,6 +94,10 @@ class BrowserHistory {
         maps[hostname] = 1;
       }
     });
+    return maps;
+  }
+  static processHistory(data) {
+    let maps=BrowserHistory.getMaps(data)
     const mapkeys = Object.keys(maps);
     const urlList = mapkeys.map(function (key, index) {
       return { label: key, value: maps[key] }
@@ -66,7 +105,19 @@ class BrowserHistory {
     urlList.sort(function (A, B) {
       return B.value - A.value;
     })
-    Charts.loadCharts(urlList.splice(0, 10), domSelector);
+    GoogleCharts.loadCharts(urlList, domSelector);
+  }
+  static processGoogleChartsHistory(data) {
+    let maps=BrowserHistory.getMaps(data)
+    const mapkeys = Object.keys(maps);
+     let urlList = mapkeys.map(function (key, index) {
+      return [ key, maps[key] ];
+    });
+    urlList.sort(function (A, B) {
+      return B.value - A.value;
+    })
+    urlList.splice(0,0,["Tasks","Browser History "])
+    GoogleCharts.loadCharts(urlList, domSelector);
   }
 }
 class View {
@@ -92,5 +143,8 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-BrowserHistory.loadBrowserHistory(1);
 window.loadBrowserHistory = BrowserHistory.loadBrowserHistory;
+const drawChart =()=>BrowserHistory.loadBrowserHistory(1);
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
